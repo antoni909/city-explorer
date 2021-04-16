03dc726f7d56f8ccbc21b241a895f970434e0a01
antoni <lorenzo.antonio.090@gmail.com>
2021-04-16 16:00:38 -0700

    fix carousel bug for movies component, implement logic for movies component, change backend request to heroku server

--- src/Movies.js
+++ src/Movies.js
@@ -4,34 +4,40 @@ import Carousel from 'react-bootstrap/Carousel';
 
 
 class Movies extends React.Component{
-
   render(){
     // console.log('this.props.movieData: ',this.props.movieData);
     console.log('this.props.movieData.data: ',this.props.movieData.data);
+
+    // Tasks:
+    //      the problem: 404 server errors: cannot retrieve some images
+    //      temp solution: placeholder image with friendly message: 'cannot retrieve image...'
+
     return(
       <>
         {this.props.display?
           <Container>
-            <Carousel >
-              Movies Related to: 'Some City Search'
+            <Carousel>
               {this.props.movieData.data.map( (element, index) => {
+
                 return(
-                  <Carousel.Item key={index}>
-                    <Carousel.Caption>
-                      <h3>{element.title}</h3>
-                      <p>{element.description}</p>
-                      <p> Total Votes: {element.votesTotal}, Average Vote: {element.voteAverage}</p>
-                      <p>Released on: {element.released}, Popularity: {element.popularity}</p>
-                    </Carousel.Caption>
+                  <Carousel.Item key={index} interval={5000}>
                     <img
                       className="d-block w-100"
-                      src={element.poster}
+                      src={`https://image.tmdb.org/t/p/w400${element.poster}`}
                       alt={element.title}
                     />
+                    <Carousel.Caption>
+                      <h2>{element.title}</h2>
+                      <p>{element.description}</p>
+                      <p> Total Votes: {element.votesTotal}</p>
+                      <p>Average Vote: {element.voteAverage}</p>
+                      <p>Released on: {element.released}</p>
+                      <p>Popularity: {element.popularity}</p>
+                    </Carousel.Caption>
                   </Carousel.Item>
                 );
-              })
-              }
+
+              })}
             </Carousel>
           </Container> : ''
         }
